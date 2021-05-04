7b789632e554865d6009b9f869cd45817a2ee03b
toki888 <shiwenyun@gmail.com>
2021-05-04 15:04:19 +0900

    add tags

--- ex02/main.tf
+++ ex02/main.tf
@@ -2,5 +2,9 @@ provider "azurerm" {}
 
 resource "azurerm_resource_group" "ex02" {
   name     = "tfbc-ex02-rg"
-  location = "${var.location}"
+  location = var.location
+  tags = {
+    WY_Location       = "Japan"
+    WY_PIC            = "Wenyun"
+  }
 }
